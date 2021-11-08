d986fb51de5c8cf23f1424a07315bb096e43f4b4
vic-shihang-li <viclee@nyu.edu>
2021-11-08 17:26:46 -0500

    bench fixes and add mpsc bench

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use core::sync::atomic::{AtomicUsize, Ordering};
 use lockfree::channel::spsc;
 use nolock::queues::spsc::unbounded;
-use rtrb::RingBuffer;
+use rtrb::{PushError, RingBuffer};
 use std::sync::{Arc, Barrier};
 use std::thread;
 use std::time::Instant;
@@ -16,8 +16,8 @@ macro_rules! make_bench {
     };
 }
 
-fn bench_nolock(num_inserts: i32) -> usize {
-    let (mut rx, mut tx) = unbounded::queue::<i32>();
+fn bench_std_mpsc(num_inserts: i32) -> usize {
+    let (tx, rx) = std::sync::mpsc::channel::<usize>();
     let start_gate = Arc::new(Barrier::new(3));
     let end_gate = Arc::new(Barrier::new(3));
 
@@ -26,7 +26,48 @@ fn bench_nolock(num_inserts: i32) -> usize {
     let t1 = thread::spawn(move || {
         t1_start.wait();
         for i in 0..num_inserts {
-            match tx.enqueue(i) {
+            match tx.send(1) {
+                Err(e) => println!("{:?}", e), // should not err here
+                Ok(_) => (),
+            }
+        }
+        drop(tx);
+        t1_end.wait();
+    });
+
+    let t2_start = start_gate.clone();
+    let t2_end = end_gate.clone();
+    let t2 = thread::spawn(move || {
+        t2_start.wait();
+        let mut _sum = 0;
+        while let Ok(val) = rx.recv() {
+            _sum += val;
+        }
+        t2_end.wait();
+    });
+
+    start_gate.wait();
+    let start = Instant::now();
+    end_gate.wait();
+    let dur = start.elapsed();
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    dur.as_millis() as usize
+}
+
+fn bench_nolock(num_inserts: i32) -> usize {
+    let (mut rx, mut tx) = unbounded::queue::<usize>();
+    let start_gate = Arc::new(Barrier::new(3));
+    let end_gate = Arc::new(Barrier::new(3));
+
+    let t1_start = start_gate.clone();
+    let t1_end = end_gate.clone();
+    let t1 = thread::spawn(move || {
+        t1_start.wait();
+        for i in 0..num_inserts {
+            match tx.enqueue(1) {
                 Err(e) => println!("{:?}", e), // should not err here
                 Ok(_) => (),
             }
@@ -58,7 +99,7 @@ fn bench_nolock(num_inserts: i32) -> usize {
 }
 
 fn bench_lockfree(num_inserts: i32) -> usize {
-    let (mut tx, mut rx) = spsc::create::<i32>();
+    let (mut tx, mut rx) = spsc::create::<usize>();
     let start_gate = Arc::new(Barrier::new(3));
     let end_gate = Arc::new(Barrier::new(3));
 
@@ -67,7 +108,7 @@ fn bench_lockfree(num_inserts: i32) -> usize {
     let t1 = thread::spawn(move || {
         t1_start.wait();
         for i in 0..num_inserts {
-            match tx.send(i) {
+            match tx.send(1) {
                 Err(e) => println!("{:?}", e), // should not err here
                 Ok(_) => (),
             }
@@ -99,7 +140,7 @@ fn bench_lockfree(num_inserts: i32) -> usize {
 }
 
 fn bench_rtrb(num_inserts: i32) -> usize {
-    let (mut tx, mut rx) = RingBuffer::new(num_inserts as usize);
+    let (mut tx, mut rx) = RingBuffer::new((num_inserts / 3) as usize);
     let start_gate = Arc::new(Barrier::new(3));
     let end_gate = Arc::new(Barrier::new(3));
 
@@ -107,10 +148,16 @@ fn bench_rtrb(num_inserts: i32) -> usize {
     let t1_end = end_gate.clone();
     let t1 = thread::spawn(move || {
         t1_start.wait();
-        for i in 0..num_inserts {
-            match tx.push(i) {
-                Err(e) => println!("{:?}", e), // should not err here
-                Ok(_) => (),
+        let mut ctr = 0;
+        for _ in 0..num_inserts {
+            'inner: loop {
+                match tx.push(1) {
+                    Ok(_) => {
+                        ctr += 1;
+                        break 'inner;
+                    }
+                    Err(PushError::Full(_)) => (),
+                }
             }
         }
         drop(tx);
@@ -122,10 +169,19 @@ fn bench_rtrb(num_inserts: i32) -> usize {
     let t2 = thread::spawn(move || {
         t2_start.wait();
         let mut _sum = 0;
-        while let Ok(val) = rx.pop() {
-            _sum += val;
+
+        loop {
+            match rx.pop() {
+                Err(_) => {}
+                Ok(i) => _sum += i,
+            }
+            if _sum == num_inserts {
+                break;
+            }
         }
+
         t2_end.wait();
+        // println!("{}", _sum);
     });
 
     start_gate.wait();
@@ -172,10 +228,12 @@ fn main() {
     const NUM_TRIALS: i32 = 1_000;
     const NUM_INSERTS: i32 = 100_000;
 
+    let std_mpsc_bench = make_bench!(bench_std_mpsc, NUM_INSERTS);
     let nolock_bench = make_bench!(bench_nolock, NUM_INSERTS);
     let lockfree_bench = make_bench!(bench_lockfree, NUM_INSERTS);
     let rtrb_bench = make_bench!(bench_rtrb, NUM_INSERTS);
 
+    bench("std::mpsc", std_mpsc_bench, NUM_TRIALS);
     bench("nolock", nolock_bench, NUM_TRIALS);
     bench("lockfree", lockfree_bench, NUM_TRIALS);
     bench("rtrb", rtrb_bench, NUM_TRIALS);
