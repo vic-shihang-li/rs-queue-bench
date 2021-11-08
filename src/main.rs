501e160d7ebbc665f6cae33bcbf2c13fc670782b
vic-shihang-li <viclee@nyu.edu>
2021-11-08 15:17:59 -0500

    implement function to benchmark lockfree crate

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::sync::atomic::{AtomicUsize, Ordering};
+use lockfree::channel::spsc;
 use nolock::queues::spsc::unbounded;
 use std::sync::{Arc, Barrier};
 use std::thread;
@@ -55,6 +56,47 @@ fn bench_nolock(num_inserts: i32) -> usize {
     dur.as_millis() as usize
 }
 
+fn bench_lockfree(num_inserts: i32) -> usize {
+    let (mut tx, mut rx) = spsc::create::<i32>();
+    let start_gate = Arc::new(Barrier::new(3));
+    let end_gate = Arc::new(Barrier::new(3));
+
+    let t1_start = start_gate.clone();
+    let t1_end = end_gate.clone();
+    let t1 = thread::spawn(move || {
+        t1_start.wait();
+        for i in 0..num_inserts {
+            match tx.send(i) {
+                Err(e) => println!("{:?}", e), // should not err here
+                Ok(_) => (),
+            }
+        }
+        drop(tx);
+        t1_end.wait();
+    });
+
+    let t2_start = start_gate.clone();
+    let t2_end = end_gate.clone();
+    let t2 = thread::spawn(move || {
+        t2_start.wait();
+        let mut _sum = 0;
+        while let Ok(val) = rx.recv() {
+            _sum += val;
+        }
+        t2_end.wait();
+    });
+
+    start_gate.wait();
+    let start = Instant::now();
+    end_gate.wait();
+    let dur = start.elapsed();
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    dur.as_millis() as usize
+}
+
 fn repeat(ntimes: i32, procedure: BenchProcedure) -> usize {
     // use worker threads to do repeated benchmark runs
     let concurrent_test_thread_count = 5;
@@ -89,6 +131,8 @@ fn main() {
     const NUM_INSERTS: i32 = 100_000;
 
     let nolock_bench = make_bench!(bench_nolock, NUM_INSERTS);
+    let lockfree_bench = make_bench!(bench_lockfree, NUM_INSERTS);
 
     bench("nolock", nolock_bench, NUM_TRIALS);
+    bench("lockfree", lockfree_bench, NUM_TRIALS);
 }
